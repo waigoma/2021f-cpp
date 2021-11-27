658e06fb235aeeadaa137944c8d9f960a8743ddb
waigoma <waichi0302@gmail.com>
2021-11-27 15:44:19 +0900

    done 3-2c

---
+++ class10/hanoi03-2c.cc
@@ -0,0 +1,33 @@
+#include "Board.h"
+
+using namespace std;
+
+int main() {
+    int diskSize;
+    int step = 0;
+    cout << "円盤の枚数> "; cin >> diskSize;
+    Board boardc(diskSize);
+    boardc.initialize();
+
+    string x, y;
+    boardc.show_aa();
+
+    while (true) {
+        cout << "hanoi> "; cin >> x;
+        if (x == "Q" || x == "q") break;
+        if (x == "R" || x == "r") {
+            boardc.initialize();
+            boardc.show_aa();
+            continue;
+        }
+        if (x == "H" || x == "h") {
+            boardc.printHelp();
+            continue;
+        }
+
+        cin >> y;
+        boardc.move(x, y);
+        boardc.show_aa();
+        if (step++ >= 99) break;
+    }
+}
