01bd8ffa387df2ac6ac053e8a8be2af98027f9a8
waigoma <waichi0302@gmail.com>
2021-12-06 15:32:58 +0900

    init class12

---
+++ class12/Board.cc
@@ -0,0 +1,80 @@
+#include "Board.h"
+
+using namespace std;
+
+// A: 5 4 3 2 1
+// B: 0 0 0 0 0
+// C: 0 0 0 0 0
+void Board::initialize() {
+    poles.clear();
+
+    Pole a("A", maxSize);
+    a.init_stack();
+    poles.push_back(a);
+
+    Pole b("B", maxSize);
+    b.clear_stack();
+    poles.push_back(b);
+
+    Pole c("C", maxSize);
+    c.clear_stack();
+    poles.push_back(c);
+}
+
+void Board::move(const std::string& fromStr, const std::string& toStr) {
+    POLE_ID fromId;
+    POLE_ID toId;
+
+    if (fromStr == "A") fromId = POLE_A;
+    else if (fromStr == "B") fromId = POLE_B;
+    else if (fromStr == "C") fromId = POLE_C;
+
+    if (toStr == "A") toId = POLE_A;
+    else if (toStr == "B") toId = POLE_B;
+    else if (toStr == "C") toId = POLE_C;
+
+    move(fromId, toId);
+}
+
+void Board::move(POLE_ID fromId, POLE_ID toId) {
+    Pole* from;
+    Pole* to;
+    bool foundFrom = false;
+    bool foundTo = false;
+    for (Pole& p : poles) {
+        if (p.getId() == fromId) {
+            from = &p;
+            foundFrom = true;
+        }
+        if (p.getId() == toId) {
+            to = &p;
+            foundTo = true;
+        }
+    }
+    if (foundFrom && foundTo) from->move_from(*to);
+    else throw std::out_of_range("Pole not found");
+}
+
+void Board::show_n_str() {
+    for (auto& p : poles) {
+        p.print();
+    }
+}
+
+void Board::show_aa() {
+    cout << right << setfill(' ') << setw(maxSize + 1) << "|" << setw(2 * maxSize + 1) << "|" << setw(2 * maxSize + 1) << "|" << endl;
+    for (int i = maxSize - 1; i >= 0; i--) {
+        for (const auto& pole : poles) {
+            pole.show_art(i);
+        }
+        cout << endl;
+    }
+    cout << right << setfill('-') << setw(maxSize + 1)  << "+" << setfill('-') << setw(2 * maxSize + 1) << "+" << setfill('-') << setw(2 * maxSize + 1) << "+" << setfill('-') << setw(maxSize + 1) << "\n";
+    cout << right << setfill(' ') << setw(maxSize + 1)  << "A" << setw(2 * maxSize + 1) << "B" << setw(2 * maxSize + 1) << "C" << setw(maxSize + 1) << "\n";
+}
+
+void Board::printHelp() {
+    cout  << left << setw(5)<< "X Y" << ": move from pole X to Y" << endl;
+    cout  << left << setw(5)<< "H" << ": show this help" << endl;
+    cout  << left << setw(5)<< "Q" << ": exit game" << endl;
+}
---
+++ class12/Pole.h
@@ -0,0 +1,42 @@
+#ifndef POLE_H
+#define POLE_H
+
+#include <vector>
+#include <stdexcept>
+#include <iomanip>
+#include "Plate.h"
+
+enum POLE_ID {
+    POLE_A,
+    POLE_B,
+    POLE_C
+};
+
+class Pole {
+protected:
+    std::string name;
+    int size;
+    const int maxSize;
+    std::vector<Plate> plates;
+    POLE_ID id;
+
+public:
+    Pole(std::string, int);
+    void init_stack();
+    void clear_stack();
+    Plate top() throw(std::out_of_range);
+    Plate pop() throw(std::out_of_range);
+    void push(Plate) throw(std::out_of_range);
+    void move_from(Pole&) throw(std::out_of_range);
+    std::string getName() const;
+    int getSize() const;
+    POLE_ID getId() const;
+    void print() const;
+    void show_art(int) const;
+};
+inline std::ostream& operator<<(std::ostream& s, const Pole& p) {
+    return s << p.getName() << "(" << p.getSize() << ")";
+}
+
+
+#endif //POLE_H
